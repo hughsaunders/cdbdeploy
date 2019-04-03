3df94d9efd2fd1c337d0458c594f9c16d3c3b668
Hugh Saunders <hugh@wherenow.org>
2019-04-03 11:25:16 +0100

    Only install chocolatey if required

--- deploy.ps1
+++ deploy.ps1
@@ -1,18 +1,31 @@
-echo "Installing Chocolatey"
+echo "Checking if deps are already installed"
+$install_sql = ! (Test-Path "HKLM:\Software\Microsoft\Microsoft SQL Server\Instance Names\SQL")
+$install_iis = ! ((Get-WindowsFeature Web-Server).InstallState -eq "Installed")
+$install_git = ! (Get-Command git)
 
-Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-
-if (Test-Path "HKLM:\Software\Microsoft\Microsoft SQL Server\Instance Names\SQL") {
-    echo "SQL Server already installed"
-} Else {
-    echo "SQL Server not found, installing"
-    choco install -y sql-server-express
+if ($install_sql -or $install_iis -or $install_git ){
+    echo "Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-echo "Installing iisexpress"
-choco install -y iisexpress
+if ($install_sql) {
+    echo "SQL Server not found, installing"
+    choco install -y sql-server-express
+} Else {
+    echo "SQL Server already installed"
+}
 
-echo "Installing git"
-choco install -y git
+if ($install_iis) {
+    echo "IIS not installed, installing iisexpress"
+    choco install -y iisexpress
+} else {
+    Write-Host "IIS is already installed"
+}
+
+if($install_git){
+    echo "Installing git"
+    choco install -y git
+} else {
+    echo "git already installed"
+}
